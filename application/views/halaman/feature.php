b8d3091a218be6d9669ec8ac6675fafe32789012
Aswita Hidayat <aswita_hidayat@yahoo.com>
2016-05-18 22:25:07 +0800

    update

--- application/views/halaman/feature.php
+++ application/views/halaman/feature.php
@@ -1,59 +1,58 @@
 <div class="inner-box relative" style="padding:10px 20px; width:100%;">
-    <h2 class="title-2" style="font-size:16px; text-align: center;"><b>KENAPA HARUS SEWANIA ?</b><hr/>
+    <h2 class="title-2 text-center" style="font-size:16px;"><b>KENAPA HARUS SEWANIA ?</b><hr/>
     <div class="row">
-        <div class="col-xs-6 col-sm-4 " >
-            <div class="cat-list">
-            <center><a ><img src="<?php echo base_url('assets/img/bottom_icon/GEO.png'); ?>" style="width:91px; height:91px;" /></a><br/><p></p><b>Geo Searching</b><br /><font style="font-size:12px;">Bisa Mencari Jasa Sewa Di Sekitar Anda</font></center>
-            
-          </div>
-      </div>
+
+        <div class="col-xs-6 col-sm-4 cat-list text-center" >
+            <a >
+                <img src="<?php echo base_url('assets/img/bottom_icon/GEO.png'); ?>" class="feature-logo" />
+            </a><br/>
+            Geo Searching<br />
+            <div class="feature">Bisa Mencari Jasa Sewa Di Sekitar Anda</div>
+        </div>
       
-      <div class="col-xs-6 col-sm-4 " >
-        <div class="cat-list">
-        
-        <center><a ><img src="<?php echo base_url('assets/img/bottom_icon/EASY TAKE.png'); ?>" style="width:91px; height:91px;" /></a><br/><p></p><b>Easy Take</b><br /><font style="font-size:12px;">Mengambil Barang Sewaan tanpa <br />harus pergi ke Tukang Sewa</font></center>
-        
-      </div>
-      </div>
+        <div class="col-xs-6 col-sm-4 cat-list text-center" >
+            <a >
+                <img src="<?php echo base_url('assets/img/bottom_icon/EASY TAKE.png'); ?>" class="feature-logo" />
+            </a><br/>
+            Easy Take<br />
+            <div class="feature">Mengambil Barang Sewaan tanpa <br />harus pergi ke Tukang Sewa</div>
+        </div>
       
-      <div class="col-xs-6 col-sm-4 " >
-        <div class="cat-list">
-        
-        <center><a ><img src="<?php echo base_url('assets/img/bottom_icon/RATING.png'); ?>" style="width:91px; height:91px;" /></a><br/><p></p><b>Rating</b><br /><font style="font-size:12px;">Memudahkan Memilih Jasa sewa  <br />dengan melihan popularitasnya</font></center>
-        
-      </div>
-      </div>
+        <div class="col-xs-6 col-sm-4 cat-list text-center" >
+            <a >
+                <img src="<?php echo base_url('assets/img/bottom_icon/RATING.png'); ?>" class="feature-logo" />
+            </a><br/>
+            <b>Rating</b><br />
+            <div class="feature">Memudahkan Memilih Jasa sewa  <br />dengan melihan popularitasnya</div>        
+        </div>
       
-      <div class="col-xs-6 col-sm-4 " >
-        <div class="cat-list">
-        
-        <center><a ><img src="<?php echo base_url('assets/img/bottom_icon/CHAT.png'); ?>" style="width:91px; height:91px;" /></a><br/><p></p><b>Chat</b><br /><font style="font-size:12px;">Berkomunikasi langsung dengan tukang <br /> sewa dengan fasilitas Chating</font></center>
-        
-      </div>
-      </div>
+        <div class="col-xs-6 col-sm-4 cat-list text-center" >
+            <a >
+                <img src="<?php echo base_url('assets/img/bottom_icon/CHAT.png'); ?>" class="feature-logo" />
+            </a><br/>
+            Chat<br />
+            <div class="feature">Berkomunikasi langsung dengan tukang <br /> sewa dengan fasilitas Chating</div>        
+        </div>
       
-      <div class="col-xs-6 col-sm-4 " >
-        <div class="cat-list">
-        
-        <center><a ><img src="<?php echo base_url('assets/img/bottom_icon/verifikasi.jpg'); ?>" style="width:91px; height:91px;" /></a><br/><p></p><b>Verifikasi</b><br /><font style="font-size:12px;">Verifikasi aman sewania</font></center>
-        
-      </div>
-      </div>
+        <div class="col-xs-6 col-sm-4 cat-list text-center" >
+            <a >
+                <img src="<?php echo base_url('assets/img/bottom_icon/verifikasi.jpg'); ?>" class="feature-logo" />
+            </a><br/>
+            Verifikasi<br />
+            <div class="feature">Verifikasi aman sewania</div>
+        </div>
       
-      <div class="col-xs-6 col-sm-4 " >
-        <div class="cat-list">
-        
-        <center><a ><img src="<?php echo base_url('assets/img/bottom_icon/REKENING BERSAMA.png'); ?>" style="width:91px; height:91px;" /></a><br/><p></p><b>Rekening Bersama</b><br /><font style="font-size:12px;">Transaksi lebih cepat dan  <br />effisien dengan rekening bersama</font></center>
-        
-      </div>
-      </div>
-      
-    </div>
-    
-    </h2>
-     
+        <div class="col-xs-6 col-sm-4 cat-list text-center" >
+            <a >
+                <img src="<?php echo base_url('assets/img/bottom_icon/REKENING BERSAMA.png'); ?>" class="feature-logo" />
+            </a><br/>
+            Rekening Bersama<br />
+            <div class="feature">Transaksi lebih cepat dan  <br />effisien dengan rekening bersama</div>        
+        </div>
+    </div>  
 </div>
-<center><b style="color:#888888;">Didukung oleh :</b> <br />
-        <img src="<?php echo base_url('assets/img/logo_bdi.jpg'); ?>" style="width:110px; height:90px;" />
-        </center>
-        <br />
+
+<div class="text-center space-bottom">
+    <b>Didukung oleh :</b> <br />
+    <img src="<?php echo base_url('assets/img/logo_bdi.jpg'); ?>" style="width:110px; height:90px;" />
+</div>
